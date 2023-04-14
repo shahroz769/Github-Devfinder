589a5aa6cf9e33e75340c932f68002930066f497
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-04-14 09:19:33 +0500

    Added User Details

--- app.js
+++ app.js
@@ -24,30 +24,77 @@ function theme() {
     }
 }
 window.getDevDetails = function () {
-
     let userName = document.querySelector("#username").value;
-
+    let gitName = document.getElementById("git-name")
+    let gitUsername = document.getElementById("git-username")
+    let gitBio = document.getElementById("git-bio")
+    let gitRepo = document.getElementById("git-repo")
+    let gitFollowers = document.getElementById("git-followers")
+    let gitFollowing = document.getElementById("git-following")
+    let gitLocation = document.getElementById("git-location")
+    let gitTwitter = document.getElementById("git-twitter")
+    let gitUrl = document.getElementById("git-url")
+    let gitCompany = document.getElementById("git-company")
+    let gitProfileImage1 = document.getElementById("git-profile-image1")
+    let gitProfileImage2 = document.getElementById("git-profile-image2")
+    let gitDate = document.getElementById("git-date")
     axios.get(`https://api.github.com/users/${userName}`)
-        .then(function (response) {
+    .then(function (response) {
             console.log(response.data);
-            document.getElementById("git-name").innerHTML = response.data.name
-            document.getElementById("git-username").innerHTML = "@" + response.data.login
-            document.getElementById("git-bio").innerHTML = response.data.bio
-            document.getElementById("git-repo").innerHTML = response.data.public_repos
-            document.getElementById("git-followers").innerHTML = response.data.followers
-            document.getElementById("git-following").innerHTML = response.data.following
-            document.getElementById("git-location").innerHTML = response.data.location
-            document.getElementById("git-twitter").innerHTML = response.data.twitter_username
-            document.getElementById("git-url").innerHTML = response.data.html_url
-            document.getElementById("git-company").innerHTML = response.data.company
-            document.getElementById("git-profile-image1").src = response.data.avatar_url
-            document.getElementById("git-profile-image2").src = response.data.avatar_url
-            document.getElementById("git-date").innerHTML = "Joined " + response.data.created_at
+            // ----------------------------------MONTH---------------------MONTH--------------------MONTH----------------------------------------------------
+            let date = response.data.created_at.replaceAll("-", " ").slice(0, 10)
+            let monthNum = date.slice(5, 7)
+            if (monthNum[0] == 0) {
+                monthNum = monthNum.slice(1, 2)
+            }
+            let monthStr = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",]
+            let month = monthStr[monthNum]
+            console.log(monthNum, month)
+            let theDate = date.slice(8, 10) + " " + month + " " + date.slice(0, 4)
+            // ----------------------------------MONTH---------------------MONTH--------------------MONTH----------------------------------------------------
+            // ----------------------------------------URL---------------------URL--------------------URL----------------------------------------------------
+            let url = response.data.html_url.slice(8)
+            // ----------------------------------------URL---------------------URL--------------------URL----------------------------------------------------
+            // ----------------------------CAPITALIZ---------------------CAPITALIZ--------------------CAPITALIZ----------------------------------------------
+            let location = []
+            if (response.data.location !== null && response.data.location !== undefined) {
+                let tempLoc = response.data.location
+                let locationArr = tempLoc.split(" ")
+                for (i = 0; i < locationArr.length; i++) {
+                    let capitalize = locationArr[i][0].toUpperCase() + locationArr[i].slice(1).toLowerCase()
+                    location.push(capitalize)
+                }
+                location = location.join(" ")
+            }
+            // ----------------------------CAPITALIZ---------------------CAPITALIZ--------------------CAPITALIZ----------------------------------------------
+            gitName.innerHTML = response.data.name
+            gitUsername.innerHTML = "@" + response.data.login
+            gitBio.innerHTML = response.data.bio
+            gitRepo.innerHTML = response.data.public_repos
+            gitFollowers.innerHTML = response.data.followers
+            gitFollowing.innerHTML = response.data.following
+            gitLocation.innerHTML = location
+            gitTwitter.innerHTML = response.data.twitter_username
+            gitUrl.innerHTML = url
+            gitUrl.href = response.data.html_url
+            gitCompany.innerHTML = response.data.company
+            gitProfileImage1.src = response.data.avatar_url
+            gitProfileImage2.src = response.data.avatar_url
+            gitDate.innerHTML = "Joined " + theDate
+            if (!gitBio.innerHTML) {
+                gitBio.innerHTML = "No Bio"
+            }
+            if (!gitTwitter.innerHTML) {
+                gitTwitter.innerHTML = "Not Available"
+            }
+            if (!gitLocation.innerHTML) {
+                gitLocation.innerHTML = "Not Available"
+            }
+            if (!gitCompany.innerHTML) {
+                gitCompany.innerHTML = "Not Available"
+            }
         })
         .catch(function (error) {
             console.log(error.data);
         })
-
-
-
 }
